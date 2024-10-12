936c241d68358e87f67370966dedc04399155148
Shweta12-s <shwetakumari03.2004@gmail.com>
2024-10-12 23:38:17 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,16 @@
+// console.log("Hiiii");
+// fullName = "Shweta";
+// console.log(fullName);
+// let name = "Shwet";
+// console.log(name);
+// let age = 14;
+// let price = 100.50;
+
+const student  = {
+    name: "Rahul Verma",
+    age: 20,
+    cgpa: 8.9,
+    isPass: true
+};
+student.name = "Shweta Sinha";
+console.log(student["name"]);
